3a2b1e7e5394a33c00d29a7aed5eb9b268bffa3a
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-09-25 23:17:54 -0400

    remove error message if valid tweet is entered

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,11 @@
 // function used to dynamically update charactor counter on tweet box
 $(document).ready(() => {
   $("#tweet-text").on("input", function(e) {
+    $("#empty-error").slideUp();
     if (e.target.value.length < 141) {
+      $("#too-long-error").slideUp();
       $("div.tweet-button-counter").find(".counter")
-        .html(e.target.value.length)
+        .html(140 - e.target.value.length)
         .css("color", "#545149");
     }
     if (e.target.value.length > 140) {
